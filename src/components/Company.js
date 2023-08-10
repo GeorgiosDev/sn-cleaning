07567841f9336d2e5e8d05c39624eae76cd9bc52
Georgios Deverakis <68283119+GeorgiosDev@users.noreply.github.com>
2023-08-10 15:55:17 +0300

    Testing

--- src/components/Company.js
+++ src/components/Company.js
@@ -5,7 +5,7 @@ import companyImg from "../assets/images/company.jpg"
 const Company = () => {
   return (
     <section className="company">
-      <h2 className="company-h2">Η εταιρεία μας</h2>
+      <h2 className="company-h2">Η εταιρεία μας!!!</h2>
       <div className="company-component">
         <img src={companyImg} alt="Company" className="company-image" />
         <p className="company-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum velit, pariatur explicabo molestiae omnis alias optio nam ea dolorem assumenda, illo quae itaque nesciunt neque natus at dolores ullam quaerat debitis. Suscipit repudiandae deserunt ullam quam doloribus? Ab, id? Blanditiis sapiente hic dolor nobis ex minima eum ipsum consectetur delectus earum quasi obcaecati in, aspernatur, quidem voluptate? Aliquam, maxime minima reiciendis ad quibusdam maiores optio.</p>
