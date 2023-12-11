7729e18404ccd55cd7da02b0f095cb442ff312e1
GeorgiosDev <georgiosdeverakis@gmail.com>
2023-12-11 09:52:49 +0200

    remove css minifier

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -43,7 +42,6 @@ module.exports = {
     minimize: true,
     minimizer: [
       new TerserPlugin(), // Minify JavaScript
-      new CssMinimizerPlugin(), // Minify CSS
     ],
     
   },
